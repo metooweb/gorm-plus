d4262a682016806d2b57a801f696ad53e8b877d7
oldtailor <1058776960@qq.com>
2018-05-16 10:40:38 +0800

    first commit

---
+++ gorm.go
@@ -0,0 +1,89 @@
+package gorm_plus
+
+import (
+	"github.com/jinzhu/gorm"
+	"database/sql"
+	"fmt"
+)
+
+type DB struct {
+	db        *gorm.DB
+	tx        *gorm.DB
+	transTime uint
+}
+
+func (t *DB) Inst() *gorm.DB {
+
+	if t.tx != nil {
+		return t.tx
+	} else {
+		return t.db
+	}
+}
+
+func (t *DB) MustBegin() {
+
+	var err error
+	t.transTime++
+	if t.tx == nil {
+		t.tx = t.db.Begin()
+		err = t.tx.Error
+	} else {
+		err = t.tx.Exec(fmt.Sprintf("SAVEPOINT metooweb_trans_%d", t.transTime)).Error
+	}
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func (t *DB) MustCommit() {
+	var err error
+	if t.tx == nil {
+		err = sql.ErrTxDone
+		return
+	}
+
+	if t.transTime == 1 {
+		err = t.tx.Commit().Error
+		t.tx = nil
+	}
+
+	t.transTime--
+
+	if err != nil {
+		panic(err)
+	}
+
+	return
+
+}
+
+func (t *DB) Rollback() {
+
+	var err error
+
+	if t.tx == nil {
+		err = sql.ErrTxDone
+		return
+	}
+
+	if t.transTime == 1 {
+		t.tx.Rollback()
+		t.tx = nil
+	} else {
+		err = t.tx.Exec(fmt.Sprintf("ROLLBACK TO metooweb_trans_%d", t.transTime)).Error
+	}
+
+	t.transTime--
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
+func NewDB(db *gorm.DB) *DB {
+
+	return &DB{db: db}
+}
